f07d09d2a5c923ba49454c3527dbbeb51f40d200
Agustin978 <loboagus10@gmail.com>
2023-04-02 15:20:53 -0300

    Quitados los comentarios y los console.log innesesarios

--- Desarrollo/js/app.js
+++ Desarrollo/js/app.js
@@ -15,10 +15,6 @@ class Tarea
     getTarea(){return this.tarea;}
     getDescripcion(){return this.descripcion;}
     get id(){return this.#id;}
-    /*
-    set tarea(nuevaTarea){this.tarea = nuevaTarea;}
-    set descripcion(nuevaDescripcion){this.descripcion = nuevaDescripcion;}
-    */
     static #incrementarID()
     {
         if(!this.lastID)
@@ -40,16 +36,12 @@ function compruebaFormulario(e)
     let tarea = datoTarea.value;
     let descripcion = datoDescripcion.value;
     formulario.reset();
-    //console.log(tarea);
-    //console.log(descripcion);
     if(tarea.length < 2)
     {
         alert('No se ingreso correctamente la tarea. Intente nuevamente.');
     }else
     {
         let objTarea = new Tarea(tarea, descripcion);
-        console.log(objTarea.getTarea());
-        console.log(objTarea.getDescripcion());
         tareas.push(objTarea);
         cargaTareas();
     }
@@ -61,22 +53,14 @@ const cargaTareas = () =>
     tareas.map(tarea => {
         tareasHTML += creaTareaHTML(tarea.id, tarea.getTarea(), tarea.getDescripcion());
     })
-    /*
-    for(let tareaIngresada in tareas)
-    {
-        tareasHTML += creaTareaHTML(tareaIngresada.id, tareaIngresada.getTarea(), tareaIngresada.getDescripcion());
-    }*/
     document.getElementById('tablaTareas').innerHTML = tareasHTML;
 }
-
-//let botones = document.getElementsByClassName();
 
 const creaTareaHTML = (id, tarea, desc) =>
 {
     let datoHTML;
     if( desc !== '')
     {
-        //datoHTML = '<tr class="fs-4 fw-bold"><th>'+id+'</th><th>'+tarea+'</th><th><button class="btn btn-outline-success btn-sm" onclick="detalle('+desc+')">+</button></th><th><button class="btn btn-outline-danger btn-sm" onclick="eliminar(tarea)">Eliminar</button></th></tr>';
         datoHTML = '<tr class="fs-4 fw-bold"><th>'+id+'</th><th>'+tarea+'</th><th><button class="btn btn-outline-success btn-sm" onclick="detalle(\''+desc+'\')">+</button></th><th><button class="btn btn-outline-danger btn-sm" onclick="eliminar(\''+id+'\')">Eliminar</button></th></tr>';
     }else
     {
